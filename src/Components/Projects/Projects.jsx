3f362d35267c8d1561bcb14ca7a1486e04a05eb7
Mckreames <114367512+Mckreames@users.noreply.github.com>
2024-06-07 12:28:27 -0400

    cards and stuff

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -8,6 +8,7 @@ import {
   CardText,
   Button,
 } from "reactstrap";
+import deezNutz from "./Imgs/deez-nutz.webp";
 
 export default function Projects() {
   return (
@@ -17,20 +18,20 @@ export default function Projects() {
       <div className="pt-5 pb-5 card-container">
         <Card
           style={{
-            width: "18rem",
+            width: "fit-content",
           }}
         >
-          <img alt="Sample" src="https://picsum.photos/300/200" />
+          <img alt="Actual Nutz" src={deezNutz} />
           <CardBody>
-            <CardTitle tag="h5">Card title</CardTitle>
+            <CardTitle tag="h5">Deez Nutz</CardTitle>
             <CardSubtitle className="mb-2 text-muted" tag="h6">
-              Card subtitle
+              Those Nutz
             </CardSubtitle>
             <CardText>
-              Some quick example text to build on the card title and make up the
-              bulk of the card‘s content.
+              Deez Nutz are, in fact, Those Nutz as well. It's not a commonly
+              known fact.
             </CardText>
-            <Button>Button</Button>
+            <Button>View Project</Button>
           </CardBody>
         </Card>
       </div>
