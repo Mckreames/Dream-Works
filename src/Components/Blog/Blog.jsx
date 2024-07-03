723a0a5bd4c056110dace343fd8c514235aa2496
Mckreames <114367512+Mckreames@users.noreply.github.com>
2024-07-02 23:34:31 -0400

    blog posts from scratch again

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -2,5 +2,19 @@ import React from "react";
 import "./Blog.css";
 
 export default function Blog() {
-  return <section className="blog-container home">Hi</section>;
+  return (
+    <>
+      <section>
+        <div className="offset-1 col-10 blog-container">
+          <div className="offset-1 col-10 blog-bubble">
+            <p>ID</p>
+            <p>post-img</p>
+            <h3>Title:</h3>
+            <h5>Date:</h5>
+            <p>Content:</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
