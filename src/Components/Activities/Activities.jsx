e4f33241b6232a127c9a7a076f76b6b77d7b99cf
Mckreames <114367512+Mckreames@users.noreply.github.com>
2024-07-29 18:58:29 -0400

    activities page and blog are gonna become one in the same

--- src/Components/Activities/Activities.jsx
+++ src/Components/Activities/Activities.jsx
@@ -1,5 +1,46 @@
 import React from "react";
+import "./Activities.css";
+import Engine from "./Imgs/Engine.png";
+import Jet from "./Imgs/Jet.png";
+import CO2 from "./Imgs/NOAA CO2.png";
 
 export default function Activities() {
-  return <div>Activities</div>;
+  return (
+    <>
+      <div className="dark-back">
+        <section className="offset-1 col-10 dark-back activity-splash">
+          <div className="offset-1 col-7 main-splash">
+            <img
+              alt="Blown out image of an engine"
+              src={Engine}
+              className="activity-imgs"
+            />
+          </div>
+          <div className="col-4 list-splash">
+            <ul className="no-bullets">
+              <li className="splash-list-item">
+                <div className="list-title">Whisper Aero</div>
+                <div className="list-date">8/8/24</div>
+              </li>
+              <div className="col-10 activity-divider"></div>
+              <li className="splash-list-item">
+                <div className="list-title">Hermeus and Quarter Horse</div>
+                <div className="list-date">4/20/24</div>
+              </li>
+              <div className="col-10 activity-divider"></div>
+              <li className="splash-list-item">
+                <div className="list-title">Bomb Drone</div>
+                <div className="list-date">9/11/24</div>
+              </li>
+              <div className="col-10 activity-divider"></div>
+              <li className="splash-list-item">
+                <div className="list-title">Peeping Tom</div>
+                <div className="list-date">6/9/24</div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
